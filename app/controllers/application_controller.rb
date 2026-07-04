207cabe6edb43d9a9dcf2feac777377a27ef61f3
Toshiya <firecracker9692@gmail.com>
2026-07-04 11:28:15 +0900

    ログイン機能の実装とログイン画面、メニュー画面の作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  before_action :authenticate_user!
+
   allow_browser versions: :modern
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  private
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.find_or_create_by!(employee_number: "1001") do |user|
+  user.name = "初期スタッフ"
+  user.password = "password"
+  user.password_confirmation = "password"
+end
+
+User.find_or_create_by!(employee_number: "1002") do |user|
+  user.name = "山田 太郎"
+  user.password = "password"
+  user.password_confirmation = "password"
+end
